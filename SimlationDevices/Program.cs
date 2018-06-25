c3b348b77dc1d437c85357f5372bc9cbbdc79468
Maxime MAURER <maxime.maurer@viacesi.fr>
2018-06-25 09:41:01 +0200

    debut

--- SimlationDevices/Program.cs
+++ SimlationDevices/Program.cs
@@ -10,6 +10,22 @@ namespace SimlationDevices
     {
         static void Main(string[] args)
         {
+            Boolean presenceSensor = false;
+            double temperatureSensor = null;
+            double lightSensor = null;
+            double atmosphericPressureSensor = null;
+            double humiditySensor = null;
+            double soundLevelSensor = null;
+            double gpsSensor = null;
+            double co2LevelSensor = null;
+            double ledCommand = null;
+            double beeperCommand = null;
+            Console.WriteLine("Begin process");
+
+
+
+
+            Console.WriteLine("End process");
         }
     }
 }
