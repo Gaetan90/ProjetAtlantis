45863cbfccf7ef14f75fd4a8c581fce17d59323b
Gaëtan LOPEZ CUTILLAS <gaetan.lopezcutillas@viacesi.fr>
2018-06-25 10:08:12 +0200

    Maj WCF + reference service Device & Calcul

---
+++ WCFWebService/App_Code/IServiceCalcul.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IService" à la fois dans le code et le fichier de configuration.
+[ServiceContract]
+public interface IServiceCalcul
+{
+
+	[OperationContract]
+	string GetCalcul(int value);
+
+
+}
+
