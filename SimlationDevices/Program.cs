99997310ec9246e7dd44f4d04ee66d1930945482
Maxime MAURER <maxime.maurer@viacesi.fr>
2018-06-25 10:11:34 +0200

    Validation avant pull

--- SimlationDevices/Program.cs
+++ SimlationDevices/Program.cs
@@ -8,24 +8,50 @@ namespace SimlationDevices
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Boolean presenceSensor = false;
-            double temperatureSensor = null;
-            double lightSensor = null;
-            double atmosphericPressureSensor = null;
-            double humiditySensor = null;
-            double soundLevelSensor = null;
-            double gpsSensor = null;
-            double co2LevelSensor = null;
-            double ledCommand = null;
-            double beeperCommand = null;
+            
+            
             Console.WriteLine("Begin process");
+            for (int i = 0; i < 10; i++)
+            {
+                bool presenceSensor = new Random().Next(100) % 2 == 0;
+                Console.WriteLine("Presence Sensor : " + presenceSensor);
 
+                double temperatureSensor;
+                Console.WriteLine("Presence Sensor : " + temperatureSensor);
+
+                double lightSensor;
+                Console.WriteLine("Light Sensor : " + lightSensor);
+
+                double atmosphericPressureSensor;
+                Console.WriteLine("Atmospheric Pressure Sensor : " + atmosphericPressureSensor);
+
+                double humiditySensor;
+                Console.WriteLine("Humidity Sensor : " + humiditySensor);
+
+                double soundLevelSensor;
+                Console.WriteLine("Sound Level Sensor : " + soundLevelSensor);
+
+                double gpsSensor;
+                Console.WriteLine("Presence Sensor : " + gpsSensor);
+
+                double co2LevelSensor;
+                Console.WriteLine("Presence Sensor : " + co2LevelSensor);
+
+                double ledCommand;
+                Console.WriteLine("Presence Sensor : " + ledCommand);
+
+                double beeperCommand;
+                Console.WriteLine("Presence Sensor : " + beeperCommand);
+
+                System.Threading.Thread.Sleep(1000);
+            }
 
 
 
             Console.WriteLine("End process");
+            Console.Read();
         }
     }
 }
