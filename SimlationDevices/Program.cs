0573d103f9834a9b5a032659a75a8c3d5cb7a120
Maxime MAURER <maxime.maurer@viacesi.fr>
2018-06-26 14:33:47 +0200

    senddata ok

--- SimlationDevices/Program.cs
+++ SimlationDevices/Program.cs
@@ -10,22 +10,131 @@ using System.Web.Script.Serialization;
 using System.Net;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Threading;
 
 namespace SimlationDevices
 {
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\nBegin Get Devices\n");
+
+            var jsonUrlDevices = new WebClient().DownloadString("http://wcfwebservice.azurewebsites.net/Service.svc/devices/devices");
+            //Console.WriteLine(jsonUrlDevices);
+            var listJsonDevice = JsonConvert.DeserializeObject<List<RootObjectJsonUrl>>(jsonUrlDevices);
+            //Console.WriteLine(listJsonDevice);
+            string adressMacDevice = "";
+            //string idDevice = "";
+            string nomDevice = "";
+            string typeDevicesDevice = "";
+            
+
+            int i = 0;
+            List<RootObjectJsonUrl> listDevice = new List<RootObjectJsonUrl>();
+
+            foreach (var urlresult in listJsonDevice)
+            {
+                i++;
+                adressMacDevice = urlresult.adressMac;
+                nomDevice = urlresult.nom;
+                typeDevicesDevice = urlresult.typeDevices.name;
+                listDevice.Add(new RootObjectJsonUrl
+                {
+                    adressMac = urlresult.adressMac,
+                    nom = urlresult.nom,
+                    typeDevices = urlresult.typeDevices
+                });
+                Console.WriteLine($"Device {i} : ");
+                Console.WriteLine($"Adresse Mac : {adressMacDevice}");
+                Console.WriteLine($"Nom Device : {nomDevice}");
+                Console.WriteLine($"Type Device : {typeDevicesDevice}");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                
+            }
+            Console.WriteLine($"Nombre de devices : {i}");
+            int j = 0;
+            foreach (var deviceList in listDevice)
+            {
+                Thread[] deviceThreads = new Thread[i];
+                deviceThreads[j] = new Thread(() => SendMetrics(deviceList.adressMac, deviceList.typeDevices.name));
+                deviceThreads[j].Start();
+            }
+
+            Console.WriteLine("\nEnd Get Devices\n");
+            Console.WriteLine("Begin metric sender");
+
+
+            SendMetrics(adressMacDevice, typeDevicesDevice);
+            Console.WriteLine("End metric sender");
+
+            Console.Read();
+        }
+        public static string GetuserDevices()
+        {
+            string userdevicelist ="";
+            //get les devices des users via une requete a définir avec gaetan
+
+            return userdevicelist;
+        }
+        public static void SendMetrics(string idDevice, string deviceType)
+        {
+            string metricSend;
+            //string metricDateDevice = "2018-06-26T07:44:09.981Z"; 
+            string metricDateDevice = DateTime.UtcNow.ToString("o"); 
+            string deviceTypeDevice = "gpsSensor"; 
+            string metricValueDevice = "N;48;51;45,81;E;2;17;15,331";
+            var newMetricSend = new Metrics
+            {
+                metricDate = metricDateDevice,
+                deviceType = deviceTypeDevice,
+                metricValue = metricValueDevice
+            };
+            var json = new JavaScriptSerializer().Serialize(newMetricSend);
+            Console.WriteLine(json);
+            Console.WriteLine($"http://wcfwebservice.azurewebsites.net/Service.svc/devices/device/{idDevice}/telemetry");
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create($"http://wcfwebservice.azurewebsites.net/Service.svc/devices/device/{idDevice}/telemetry");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+            
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                Console.WriteLine(result);
+            }
+            Console.WriteLine(" ");
+            System.Threading.Thread.Sleep(1000);
+            metricSend = json;
+            //return metricSend;
+        }
+
+        public static void test()
+        {
+            ServiceDeviceClient service = new ServiceDeviceClient();
+            ICollection<DeviceView> devices = service.GetAllDevice();
+            Console.Read();
+        }
+
+    }
     public class Employee
     {
         public int id { get; set; }
         public string nom { get; set; }
         public string prenom { get; set; }
     }
-
     public class TypeDevices
     {
         public int id { get; set; }
         public string name { get; set; }
     }
-
     public class RootObjectJsonUrl
     {
         public string adressMac { get; set; }
@@ -46,111 +155,5 @@ namespace SimlationDevices
         public string deviceType;
         public string metricValue;
     }
-    class Program
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("\nBegin Get Devices\n");
-
-            var jsonUrlDevices = new WebClient().DownloadString("http://wcfwebservice.azurewebsites.net/Service.svc/devices/devices");
-            //Console.WriteLine(jsonUrlDevices);
-            var listJsonDevice = JsonConvert.DeserializeObject<List<RootObjectJsonUrl>>(jsonUrlDevices);
-            //Console.WriteLine(listJsonDevice);
-            string adressMacDevice = "";
-            string idDevice = "";
-            string nomDevice = "";
-            string typeDevicesDevice = "";
-            int i = 0;
-            foreach (var urlresult in listJsonDevice)
-            {
-                i++;
-                adressMacDevice = urlresult.adressMac;
-                nomDevice = urlresult.nom;
-                typeDevicesDevice = urlresult.typeDevices.name;
-                Console.WriteLine($"Device {i} : ");
-                Console.WriteLine($"Adresse Mac : {adressMacDevice}");
-                Console.WriteLine($"Nom Device : {nomDevice}");
-                Console.WriteLine($"Type Device : {typeDevicesDevice}");
-                Console.WriteLine("");
-                Console.WriteLine("");
-
-            }
-            Console.WriteLine("\nEnd Get Devices\n");
-
-            Console.WriteLine("Begin metric sender");
-            SendMetrics(adressMacDevice);
-            Console.WriteLine("End metric sender");
-
-            Console.Read();
-
-            /*var values = new Dictionary<string, string>{
-               { "result", "["+result+"]" },
-               { "name", dataMetrics.First().metric.nameTypeDivice },
-               { "description", "Description à affiner" }
-            };
-            HttpClient client = new HttpClient();
-            var content = new FormUrlEncodedContent(values);
- 
-            var response = await client.PostAsync("http://www.example.com/recepticle.aspx", content);
- 
-            var responseString = await response.Content.ReadAsStringAsync();
-            Console.Read();
-             
-             */
-        }
-        public static string GetuserDevices()
-        {
-            string userdevicelist ="";
-
-
-            return userdevicelist;
-        }
-        public static string SendMetrics(string idDevice)
-        {
-            string metricSend;
-            string metricDateDevice = ""; 
-            string deviceTypeDevice = ""; 
-            string metricValueDevice = "";
-            var newMetricSend = new Metrics
-            {
-                metricDate = metricDateDevice,
-                deviceType = deviceTypeDevice,
-                metricValue = metricValueDevice
-            };
-            var json = new JavaScriptSerializer().Serialize(newMetricSend);
-            //Console.WriteLine(json);
-            Console.WriteLine($"http://wcfwebservice.azurewebsites.net/Service.svc/devices/devices/{idDevice}");
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create($"http://wcfwebservice.azurewebsites.net/Service.svc/devices/devices/{idDevice}");
-            httpWebRequest.ContentType = "application/json";
-            httpWebRequest.Method = "POST";
-            /*
-            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
-            {
-                streamWriter.Write(json);
-                streamWriter.Flush();
-                streamWriter.Close();
-                Console.WriteLine(streamWriter);
-
-            }
-            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-            {
-                var result = streamReader.ReadToEnd();
-            }*/
-            Console.WriteLine(" ");
-            System.Threading.Thread.Sleep(1000);
-            metricSend = json;
-            return metricSend;
-        }
-
-        public static void test()
-        {
-            ServiceDeviceClient service = new ServiceDeviceClient();
-            ICollection<DeviceView> devices = service.GetAllDevice();
-            Console.Read();
-        }
-
-    }
-
 
 }
