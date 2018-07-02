38cab0df8f28484bc0b104c30e2a277eb8998ff7
Maxime MAURER <maxime.maurer@viacesi.fr>
2018-07-02 12:56:53 +0200

    Update Calculation + Simulation

---
+++ CalculationEngine/Model/Metrics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationEngine.Model
+{
+    public class Metrics
+    {
+        public string id;
+        public string metric;
+        public string value;
+    }
+}
